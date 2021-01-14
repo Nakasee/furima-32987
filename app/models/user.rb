5943e43653e97b94a47395948bca70cf0b8ddac1
Nakasee <yuichi.6011@gmail.com>
2021-01-15 00:30:06 +0900

    パスワード半角英数字６文字以上を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true
   validates :encrypted_password, presence: true
   validates :family_name, presence: true
   validates :first_name, presence: true
   validates :family_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,12}\z/
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
   
-
 end

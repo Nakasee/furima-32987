f2f0888dd4401ed95a34dec74ae048f2d506f4ca
Nakasee <yuichi.6011@gmail.com>
2021-01-16 15:01:38 +0900

    モデル単体のテストコードを書く

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :encrypted_password, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+    validates :family_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: {with: /\A[ァ-ヶ]+\z/} do
+  validates :family_name_kana
+  validates :first_name_kana
+  end
+
   validates :birthday, presence: true
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,12}\z/
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])\w{6,12}\z/ }
   
 end

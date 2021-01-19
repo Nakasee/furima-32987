7426665dcaf011baafde9cc9a4e7f494cc31d6fe
Nakasee <yuichi.6011@gmail.com>
2021-01-19 11:51:17 +0900

    ActiveHashモデル作成（プルダウン）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,19 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.all.order("created_at DESC")
   end
 
   def new
+   @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+  end
+
+private
+
+  def item_params
+    paramus.require(:item).permit(:name, :price, :description, :category_id, :judgment_id, :cost_id, :prefecture_id, :day_id )
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,25 @@
 class Item < ApplicationRecord
+  
+  belongs_to :user
+  belongs_to :category
+  belongs_to :judgment
+  belongs_to :cost
+  belongs_to :prefecture
+  belongs_to :day
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  with_options presence: true do
+    validates :name
+    validates :price, format: { with: /\A[0-9]+\z/ },numericality: { greater_than: 299, less_than: 9_999_999 }
+    validates :discription
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :judgment_id
+      validates :cost_id
+      validates :prefecture_id
+      validates :day_id
+    end
+  
+  end
 end

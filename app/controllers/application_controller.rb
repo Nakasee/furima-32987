80df8cb7a15063fa41c5400dcd27ffbadf81bdff
Nakasee <yuichi.6011@gmail.com>
2021-01-21 19:08:26 +0900

    rubocop起動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:nickname, :family_name, :first_name,
                                              :family_name_kana, :first_name_kana, :birthday])
- 
   end
 end

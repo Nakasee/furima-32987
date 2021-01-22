d68bb249335ed2c51e989390e141b0696c9ec41e
Nakasee <yuichi.6011@gmail.com>
2021-01-22 14:51:33 +0900

    商品編集機能実装（Rubocop起動済み)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -18,13 +21,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
   end
 
   def destory
@@ -35,5 +42,13 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :price, :image, :description, :category_id, :judgment_id, :cost_id, :prefecture_id,
                                  :day_id).merge(user_id: current_user.id)
+  end
+
+  def contributor_confirmation
+    redirect_to root_path(current_user) unless Item.find(params[:id]).user.id == current_user.id
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
